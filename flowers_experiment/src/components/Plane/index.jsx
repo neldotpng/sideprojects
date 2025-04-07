ad96a95f29bffe975a3ce97847039df5050e2b0b
Nelson Chang <neldotpng@users.noreply.github.com>
2025-04-07 18:25:52 -0400

    wip

--- flowers_experiment/src/components/Plane/index.jsx
+++ flowers_experiment/src/components/Plane/index.jsx
@@ -3,10 +3,25 @@ import vertexShader from "./shaders/vertexShader.glsl?raw";
 import fragmentShader from "./shaders/fragmentShader.glsl?raw";
 import { useEffect, useRef, useMemo } from "react";
 import * as THREE from "three";
+import { useControls } from "leva";
 
 const Plane = ({ segments = 128 }) => {
   const { viewport, size } = useThree();
   const plane = useRef();
+  const { progress, testValue } = useControls("Uniforms", {
+    progress: {
+      value: 0,
+      min: 0,
+      max: 1,
+      step: 0.001,
+    },
+    testValue: {
+      value: 0,
+      min: -1,
+      max: 1,
+      step: 0.001,
+    },
+  });
 
   useEffect(() => {
     plane.current.scale.x = viewport.width;
@@ -18,12 +33,16 @@ const Plane = ({ segments = 128 }) => {
       uTime: { value: 0 },
       uResolution: { value: new THREE.Vector2(size.width, size.height) },
       uMouse: { value: new THREE.Vector2(0, 0) },
+      uProgress: { value: 0 },
+      uTest: { value: 0 },
     }),
     [size]
   );
 
   useFrame(({ clock }) => {
     uniforms.uTime.value = clock.getElapsedTime();
+    uniforms.uProgress.value = progress;
+    uniforms.uTest.value = testValue;
   });
 
   const onPointerMove = (e) => {
--- flowers_experiment/src/components/Plane/shaders/fragmentShader.glsl
+++ flowers_experiment/src/components/Plane/shaders/fragmentShader.glsl
@@ -1,12 +1,40 @@
 uniform float uTime;
 uniform vec2 uResolution;
 uniform vec2 uMouse;
+uniform float uProgress;
+uniform float uTest;
 
 varying vec2 vUv;
 
+// p : center point
+// r : petal length
+// n : number of petals in increments of 2
+// d : central diamenter
+vec3 flower2D(vec2 p, float r, float n, float d) {
+  float s = length(p) / (r * .5);
+  float a = atan(p.x, p.y);
+
+  float f = cos(a * n);
+  f = abs(f) + d;
+
+  return vec3(1. - smoothstep(f, f+.02, s));
+}
+
 void main() {
   vec2 mouse = uMouse / uResolution;
+  vec3 color = vec3(0.);
+  vec2 pos = vUv - mouse;
+  pos = vUv - vec2(.5);
+  float t = pow(abs(sin((uTime) * .5)), 3.);
 
-  vec3 color = vec3(mouse, 1.);
+  vec3 flower1 = flower2D(pos, .5, 3. + t * 3., .5);
+  flower1 *= vec3(1., 1., 0.);
+
+  // vec3 flower2 = flower2D(pos, .2, 5., 0.3);
+  // vec3 flower2 = flower2D(pos, .5 * t2, 3. + t2 * 3., .5);
+  // flower2 *= vec3(1., 0., 1.);
+
+  color = flower1;
+
   gl_FragColor = vec4(color, 1.);
 }
