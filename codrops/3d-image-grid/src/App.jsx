7fe04f8973d29d84d32ea730b9951c07987f9874
Nelson Chang <neldotpng@users.noreply.github.com>
2025-04-11 22:35:34 -0400

    remove unused

--- codrops/3d-image-grid/src/App.jsx
+++ codrops/3d-image-grid/src/App.jsx
@@ -1,6 +1,5 @@
 import { Canvas } from "@react-three/fiber";
 import Debug from "./components/Debug";
-import FullscreenShaderPlane from "./components/FullscreenShaderPlane";
 import Grid from "./components/Grid";
 
 const App = () => {
@@ -11,16 +10,11 @@ const App = () => {
         camera={{ near: 0.1, far: 1000, position: [0, 0, 10] }}>
         <Debug />
 
-        <ambientLight intensity={0.1} />
-        <directionalLight
-          color="red"
-          position={[0, 0, 5]}
-        />
         <Grid
           imageSize={20}
           margins={50}
           imageScale={4}
-          hoverRadius={10}
+          hoverRadius={8}
         />
       </Canvas>
     </main>
