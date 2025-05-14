91c8acfb647e5c50a53ddf6394d5c2363b77abf2
Nelson Chang <neldotpng@users.noreply.github.com>
2025-05-14 15:35:17 -0400

    boundaries wip

--- fluids/src/global/hooks/useLiquidBuffer/shaders/boundary.frag
+++ fluids/src/global/hooks/useLiquidBuffer/shaders/boundary.frag
@@ -1,26 +1,12 @@
-precision highp float;
-
-uniform sampler2D uVelocity;
+uniform sampler2D uTexture;
 uniform vec2 uCellScale;
+uniform vec2 uOffset;
+uniform vec2 uScale;
 
 varying vec2 vUv;
 
 void main() {
-  vec2 velocity = texture2D(uVelocity, vUv).xy;
+  vec3 color = uScale * texture2D(uTexture, vUv + (uOffset * uCellScale));
 
-  bool isLeft   = vUv.x < uCellScale.x * 2.;
-  bool isRight  = vUv.x > 1.0 - uCellScale.x * 2.;
-  bool isBottom = vUv.y < uCellScale.y * 2.;
-  bool isTop    = vUv.y > 1.0 - uCellScale.y * 2.;
-
-  // Reflect velocity at the boundaries
-  if (isLeft || isRight) {
-    velocity.x *= -1.0;
-  }
-
-  if (isBottom || isTop) {
-    velocity.y *= -1.0;
-  }
-
-  gl_FragColor = vec4(velocity, 0.0, 1.0);
+  gl_FragColor = vec4(color, 1.);
 }
--- fluids/src/global/hooks/useLiquidBuffer/useBoundary.jsx
+++ fluids/src/global/hooks/useLiquidBuffer/useBoundary.jsx
@@ -1,41 +1,28 @@
 import { useFrame } from "@react-three/fiber";
 import { useMemo } from "react";
-import { useFBO } from "@react-three/drei";
-import { LinearFilter, FloatType, RGBAFormat, Uniform } from "three";
+import { Uniform } from "three";
 
 import useShaderPass from "./useShaderPass";
 import boundaryFrag from "./shaders/boundary.frag?raw";
 
-const useBoundary = ({
-  cellScale,
-  resolution = 256,
-  options = {
-    stencilBuffer: false,
-    depthBuffer: false,
-    minFilter: LinearFilter,
-    magFilter: LinearFilter,
-    type: FloatType,
-    format: RGBAFormat,
-  },
-  inputTexture,
-}) => {
-  const boundary = useFBO(resolution, resolution, options);
-
+const useBoundary = ({ cellScale, offset, scale, inputFBO, outputFBO }) => {
   const uniforms = useMemo(() => {
     return {
       uCellScale: new Uniform(cellScale),
-      uVelocity: new Uniform(inputTexture.current),
+      uTexture: new Uniform(inputFBO.texture),
+      uOffset: new Uniform(offset),
+      uScale: new Uniform(scale),
     };
-  }, [cellScale, inputTexture]);
+  }, [cellScale, offset, scale, inputFBO]);
 
   const boundaryRef = useShaderPass({
     fragmentShader: boundaryFrag,
     uniforms,
-    fbo: boundary,
+    fbo: outputFBO,
   });
 
   useFrame(() => {
-    uniforms.uVelocity.value = inputTexture.current;
+    uniforms.uVelocity.value = inputFBO.texture;
   });
 
   return boundaryRef;
